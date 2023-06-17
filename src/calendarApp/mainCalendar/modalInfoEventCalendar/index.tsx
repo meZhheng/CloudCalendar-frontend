df2324c226ab2cb0e8eb654916c83a1a9381c9dd
bloom667 <76032827+bloom667@users.noreply.github.com>
2023-06-17 16:30:47 +0800

    获取组ID到日历界面

--- src/calendarApp/mainCalendar/modalInfoEventCalendar/index.tsx
+++ src/calendarApp/mainCalendar/modalInfoEventCalendar/index.tsx
@@ -1,5 +1,6 @@
-import { Button, Modal, TextField } from '@mui/material';
-import { Key, useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Modal, Container, TextField, Button } from '@mui/material';
+import { Key } from 'react';
 import { toast } from 'react-toastify';
 import { ListColorsCard } from '../../types';
 import {
@@ -7,7 +8,8 @@ import {
   deleteEventCalendar,
   updateEventCalendar,
 } from '../../services/eventCalendarApi';
-import {BackgroundColorRounded, BoxContainer, SelectColors} from './styles';
+import { BackgroundColorRounded, BoxContainer, SelectColors } from './styles';
+import { useGetGroupIdMutation } from "../../../store/shareGroupIdApi";
 
 interface ICardColor {
   backgroundColor: string;
@@ -22,16 +24,33 @@ interface IModalInfosEventCalendaryProps {
 }
 
 export const ModalInfosEventCalendar = ({
-                                          handleClose,
-                                          open,
-                                          eventInfos,
-                                          isEditCard,
-                                        }: IModalInfosEventCalendaryProps) => {
+  handleClose,
+  open,
+  eventInfos,
+  isEditCard,
+}: IModalInfosEventCalendaryProps) => {
   const [title, setTitle] = useState<string>('');
   const [cardColor, setCardColor] = useState<ICardColor>({
     backgroundColor: '#039be5',
     textColor: '#ffffff',
   });
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const [ fetchGetGroupId ] = useGetGroupIdMutation();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const groupId = await fetchGetGroupId({}).unwrap();
+        console.log('Received groupId from backend:', groupId);
+        // 在这里处理 groupId，可以存储到状态中或进行其他操作
+      } catch (error) {
+        console.error('Error fetching groupId:', error);
+      }
+    };
+    if(open) {
+      fetchData();
+    }
+  }, [open]);
 
   useEffect(() => {
     if (isEditCard) {
@@ -42,7 +61,7 @@ export const ModalInfosEventCalendar = ({
       });
     } else {
       setTitle('');
-      setCardColor({backgroundColor: '#039be5', textColor: '#ffffff'});
+      setCardColor({ backgroundColor: '#039be5', textColor: '#ffffff' });
     }
   }, [eventInfos, isEditCard]);
 
@@ -53,17 +72,26 @@ export const ModalInfosEventCalendar = ({
     });
   };
 
+  const handleCheckboxChange = (option: string) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((item) => item !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+    }
+  };
+
   const handleAddedEvent = async () => {
     try {
       const calendarApi = eventInfos.view.calendar;
 
       const eventCalendar = await createEventCalendar({
         eventCalendar: {
-          title: title === '' ? 'Sem título' : title,
+          title: title === '' ? '无标题' : title,
           start: eventInfos.startStr,
           end: eventInfos.endStr,
           backgroundColor: cardColor.backgroundColor,
           textColor: cardColor.textColor,
+          selectedOptions: selectedOptions,
         },
       });
 
@@ -76,21 +104,23 @@ export const ModalInfosEventCalendar = ({
         textColor: cardColor.textColor,
       });
     } catch (err) {
-      toast.error('Houve um erro ao criar um evento');
+      toast.error('There was an error creating an event');
     } finally {
       setTitle('');
+      setSelectedOptions([]);
       handleClose();
     }
   };
 
   const handleDeleteEvent = async () => {
     try {
-      await deleteEventCalendar({id: eventInfos.event.id});
+      await deleteEventCalendar({ id: eventInfos.event.id });
       eventInfos.event.remove();
     } catch (error) {
-      toast.error('Houve um erro ao deletar o evento');
+      toast.error('There was an error deleting an event');
     } finally {
       setTitle('');
+      setSelectedOptions([]);
       handleClose();
     }
   };
@@ -102,27 +132,29 @@ export const ModalInfosEventCalendar = ({
       const eventCalendarUpdated = {
         eventCalendar: {
           _id: eventInfos.event.id,
-          title: title !== '' ? title : 'Sem título',
+          title: title !== '' ? title : '无标题',
           start: eventInfos.event.startStr,
           end: eventInfos.event.endStr,
           backgroundColor: cardColor.backgroundColor,
           textColor: cardColor.textColor,
+          selectedOptions: selectedOptions,
         },
       };
 
       const currentEvent = calendarApi.getEventById(eventInfos.event.id);
 
       if (currentEvent) {
-        currentEvent.setProp('title', title !== '' ? title : 'Sem título');
+        currentEvent.setProp('title', title !== '' ? title : '无标题');
         currentEvent.setProp('backgroundColor', cardColor.backgroundColor);
         currentEvent.setProp('textColor', cardColor.textColor);
       }
 
       await updateEventCalendar(eventCalendarUpdated);
     } catch (error) {
-      toast.error('Houve um erro ao atualizar o evento');
+      toast.error('更新事件时出现错误"');
     } finally {
       setTitle('');
+      setSelectedOptions([]);
       handleClose();
     }
   };
@@ -130,7 +162,7 @@ export const ModalInfosEventCalendar = ({
   return (
     <Modal open={open} onClose={handleClose}>
       <BoxContainer>
-        <TextField label={'Adicionar título'} value={title} onChange={(e) => setTitle(e.target.value)} fullWidth/>
+        <TextField label={'日程计划'} value={title} onChange={(e) => setTitle(e.target.value)} fullWidth />
 
         <SelectColors>
           {ListColorsCard.map((color: ICardColor, index: Key | null | undefined) => (
@@ -149,18 +181,30 @@ export const ModalInfosEventCalendar = ({
           ))}
         </SelectColors>
 
+        {/* 根据后端数据生成复选框选项 */}
+        {selectedOptions.map((option:any) => (
+          <div key={option.id}>
+            <input
+              type="checkbox"
+              checked={selectedOptions.includes(option.id)}
+              onChange={() => handleCheckboxChange(option.id)}
+            />
+            <label>{option.label}</label>
+          </div>
+        ))}
+
         <Button
           variant="contained"
           fullWidth
           onClick={isEditCard ? handleUpdatedEvent : handleAddedEvent}
           sx={{ marginTop: '0.5rem' }}
         >
-          {isEditCard ? 'Atualizar evento' : 'Adicionar evento'}
+          {isEditCard ? '更新事件' : '添加事件'}
         </Button>
 
         {isEditCard && (
           <Button variant="contained" fullWidth sx={{ marginTop: '0.5rem' }} onClick={handleDeleteEvent}>
-            Excluir evento
+            删除事件
           </Button>
         )}
       </BoxContainer>
